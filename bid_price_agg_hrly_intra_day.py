729b8372e3aa4086d55e0e394261336477cac129
ErRahul9 <rparashar@mountain.com>
2025-01-16 15:50:18 -0500

    fixed datetime

--- bid_price_agg_hrly_intra_day.py
+++ bid_price_agg_hrly_intra_day.py
@@ -58,7 +58,7 @@ class BidderLogAggregationHour:
 
     @property
     def s3_path_out(self):  # type: ignore[no-untyped-def]
-        S3Path = f"s3://mntn-data-archive-{self.env}/bid_price_log_agg_hrly_intraday_v2/"
+        S3Path = f"s3://mntn-data-archive-{self.env}/bid_price_log_agg_v2/"
         return S3Path
 
     @property
