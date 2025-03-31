8a45c63064098f8709b8c2590916d07d1bd7f02c
ErRahul9 <rparashar@mountain.com>
2025-03-30 22:52:04 -0400

    trying to debug the code for epoch column failures

--- bid_price_agg_hrly_intra_day.py
+++ bid_price_agg_hrly_intra_day.py
@@ -161,33 +161,18 @@ class BidderLogAggregationHour:
         bucket, key = s3_uri.replace("s3://", "").split("/", 1)
         return self.s3_path_exists(bucket, key)
 
-    # def _get_bidder_logs_df(self) -> Dict[str, List[DataFrame]]:
-    #     logger.log(self.log_level, "Begin '_get_bidder_logs_df'")
-    #     logger.log(self.log_level, "Begin '_bid_price_log_aggregation'")
-    #     valid_paths = [path for path in self.s3_path if self.path_exists(path)]
-    #     missed_files = [missed for missed in self.s3_path if not self.path_exists(missed)]
-    #     for paths in valid_paths:
-    #         print(paths)
-    #
-    #     logger.info(f"following partitions are missing in S3 {missed_files}")
-    #     for paths in valid_paths:
-    #         print(paths)
-    #
-    #     if valid_paths:
-    #         df_bid_select = self.spark.read.option("basePath", self.base_path).parquet(*valid_paths)
-    #
-    #     logger.info(f"aggregating  {valid_paths}")
     def _get_bidder_logs_df(self) -> Dict[str, List[DataFrame]]:
         logger.log(self.log_level, "Begin '_get_bidder_logs_df'")
         logger.log(self.log_level, "Begin '_bid_price_log_aggregation'")
-        valid_paths = [path for path in self.s3_path if self.path_exists(path)]
+        # valid_paths = [path for path in self.s3_path if self.path_exists(path)]
+        valid_paths = [path for path in self.s3_path]
         missed_files = [missed for missed in self.s3_path if not self.path_exists(missed)]
         for paths in valid_paths:
             print(paths)
+        for missed in missed_files:
+            print(f'{missed} files are missing')
 
         logger.info(f"following partitions are missing in S3 {missed_files}")
-        for paths in valid_paths:
-            print(paths)
 
         if valid_paths:
             df_bid_select = self.spark.read.option("basePath", self.base_path).parquet(*valid_paths)
